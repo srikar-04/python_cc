640095107d68f3711e172e7408a8f91316859402
srikar <srikar.code01@gmail.com>
2025-03-30 17:54:02 +0530

    refactor debugging decorator to improve argument logging and update example function output

--- 07_decorators/debugging_funciton_calls.py
+++ 07_decorators/debugging_funciton_calls.py
@@ -3,13 +3,15 @@
 def func_details(func):
     def wrapper(*args, **kwargs):
         function_name = func.__name__
-        function_arguments = [args, kwargs]
-        print(f'function name and arguments are {function_name}, {function_arguments}')
-        return [function_name, function_arguments]
+        args_value = ", ".join(str(arg) for arg in args)
+        kwargs_value = ', '.join(f'{k}:{v}' for k, v in kwargs.items())
+        # print(f'function name and arguments are {function_name} and {function_arguments}')
+        print(f'arguments are {args_value} and kw_arguments are {kwargs_value}')
+        return func(*args, **kwargs)
     return wrapper
 
 @func_details
 def example_func(num_1, num_2):
-    print(f'this function arguments are, {num_1, num_2}')
+    print('Hello from example function')
 
 example_func(2, 5)
